801f01506029c693a4f08651f8ee112c6e9beeb9
wmcdonald404 <wmcdonald@gmail.com>
2024-11-11 14:41:15 +0000

    Auto-confirm apt-get upgrade.

--- .devcontainer/bin/onCreateCommand.sh
+++ .devcontainer/bin/onCreateCommand.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 apt-get update
-apt-get upgrade
+apt-get -y upgrade
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)" --unattended
